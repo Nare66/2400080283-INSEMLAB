644171afef5b0de353a9b9ba11acd96ccad51eeb
Thoka-Narendra <2400080283@kluniversity.in>
2025-10-16 19:10:25 +0530

    feature1: Alice's change

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import React from 'react';
-
 const App = () => {
   return (
     <div>
-      <h1>Hello Git!</h1>
+      <h1>Hello from Alice!</h1>
     </div>
   );
-}
-
-export default App;
+};
